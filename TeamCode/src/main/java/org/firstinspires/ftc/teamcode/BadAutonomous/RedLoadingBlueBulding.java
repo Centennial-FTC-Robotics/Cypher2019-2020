f1d6dc523a87480a2da591beff2ef2a827412c9b
BC849 <cbryan2003@hotmail.com>
2020-01-24 17:27:40 -0500

    Disabled every bad autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BadAutonomous/RedLoadingBlueBulding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BadAutonomous/RedLoadingBlueBulding.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.BadAutonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.CypherAutoMethods;
 import org.firstinspires.ftc.teamcode.StopException;
-
+@Disabled
 @Autonomous(name = "Red Loading/Blue Building EMERG", group = "Auto")
 public class RedLoadingBlueBulding extends CypherAutoMethods {
     @Override
