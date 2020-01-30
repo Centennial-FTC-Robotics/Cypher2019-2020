300ef93aa08c618f95f8624c10ecae3544107562
LuqDude <luqmanforge@gmail.com>
2020-01-29 19:39:16 -0500

    organized autos more made different autos for moving foundation and stuff lets test like eveyrthing next meeting so auto can be done by saterday

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BetterAutonomous/Red/RedBridgeMoveFoundation.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.BetterAutonomous.Red;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.CypherAutoMethods;
+import org.firstinspires.ftc.teamcode.StopException;
+
+@Autonomous(name = "Red Bottom Move Foundation", group = "Red Bridge Auto")
+public class RedBridgeMoveFoundation extends CypherAutoMethods {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        try {
+            initEverything();
+        } catch (StopException e) {
+            stopEverything();
+        }
+        waitForStart();
+        getFoundation(-1,Side.BRIDGE);
+
+
+    }
+}
